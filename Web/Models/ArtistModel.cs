17b51ca436d1f8359cfb1040bb167ebf9293322d
Aniket Braganza <aabragan@gmail.com>
2011-05-20 19:56:31 -0400

    Added properties for Artist Model

--- Web/Models/ArtistModel.cs
+++ Web/Models/ArtistModel.cs
@@ -9,5 +9,17 @@ namespace Web.Models
     {
         public int Id { get; set; }
         public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address1 { get; set; }
+        public string Address2 { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zip { get; set; }
+        public string CellPhone { get; set; }
+        public string Email { get; set; }
+        public DateTime DOB { get; set; }
+        public Uri Website { get; set; }
+        public string Twitter { get; set; }
+        public string Facebook { get; set; }
     }
 }
