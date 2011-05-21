db234aa696c99a1e0f1dc69c50446c1c9f92991e
William Wallace <wallace.357@osu.edu>
2011-05-20 22:20:30 -0400

    Created VenueModel

--- Web/Models/VenueModel.cs
+++ Web/Models/VenueModel.cs
@@ -7,5 +7,8 @@ namespace Web.Models
 {
     public class VenueModel
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
     }
 }
