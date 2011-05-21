965fac5a63cc964ecc5d0a5914ed61b58399120f
Aniket Braganza <aabragan@gmail.com>
2011-05-20 20:46:20 -0400

    Added more properties

--- Web/Models/ArtistModel.cs
+++ Web/Models/ArtistModel.cs
@@ -8,18 +8,33 @@ namespace Web.Models
     public class ArtistModel
     {
         public int Id { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string Address1 { get; set; }
-        public string Address2 { get; set; }
-        public string City { get; set; }
-        public string State { get; set; }
-        public string Zip { get; set; }
-        public string CellPhone { get; set; }
-        public string Email { get; set; }
-        public DateTime DOB { get; set; }
-        public Uri Website { get; set; }
-        public string Twitter { get; set; }
-        public string Facebook { get; set; }
+        
+        public Entry<string> FirstName { get; set; }
+        public Entry<string> LastName { get; set; }
+        public Entry<string> Address1 { get; set; }
+        public Entry<string> Address2 { get; set; }
+        public Entry<string> City { get; set; }
+        public Entry<string> State { get; set; }
+        public Entry<string> Zip { get; set; }
+        public Entry<string> CellPhone { get; set; }
+        public Entry<string> Email { get; set; }
+        public Entry<DateTime> Dob { get; set; }
+        
+        public Entry<Uri> Website { get; set; }
+        public Entry<string> Twitter { get; set; }
+        public Entry<string> Facebook { get; set; }
+        
+        public Entry<bool> ResidentOfWesterville { get; set; }
+        public Entry<bool> WorkInWesterville { get; set; }
+        public Entry<bool> BelongToGroups { get; set; }
+        public IQueryable<string> Groups { get; set; }
+        public IQueryable<string> Awards { get; set; }
+        public Entry<bool> AttendSchoolInWesterville { get; set; }
+        public IQueryable<School> Schools { get; set; }
+
+        public Entry<bool> ImageProvided { get; set; }
+
+        public Entry<string> Comments { get; set; }
     }
+
 }
---
+++ Web/Models/School.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Web.Models
+{
+    public class School
+    {
+        public string Name { get; set; }
+        public DateTime Graduation { get; set; }
+    }
+}
