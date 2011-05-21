fee970f9391e9969a923aefc5a679f31e4f21573
Shawn Wallace <shawnewallace@gmail.com>
2011-05-20 20:26:28 -0400

    added venue controller and index test class

---
+++ Web.Tests/Controllers/VenueControllerTests/IndexTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Web.Controllers;
+
+namespace Web.UI.Tests.Controllers.VenueControllerTests
+{
+    [TestFixture]
+    public class IndexTests
+    {
+        private VenueController _controller;
+
+        [SetUp]
+        public void Setup()
+        {
+            _controller = new VenueController();
+        }
+
+        [Test]
+        public void Smoke_Test()
+        {
+            var result = _controller.Index();
+        }
+    }
+}
