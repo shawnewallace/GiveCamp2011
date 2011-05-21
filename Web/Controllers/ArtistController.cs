19caa447f71b58ae72d6fe82daea0e877564cc6c
William Wallace <wallace.357@osu.edu>
2011-05-21 19:35:10 -0400

    Created Upload art partial

--- Web/Controllers/ArtistController.cs
+++ Web/Controllers/ArtistController.cs
@@ -6,10 +6,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Lib.Common;
-<<<<<<< HEAD
 using Telerik.Web.Mvc;
-=======
->>>>>>> 9a70e7daccb592930f3d89606a68fd5beae33a3a
 using Web.Models;
 
 namespace Web.Controllers
@@ -40,13 +37,11 @@ namespace Web.Controllers
             }
         }
 
-<<<<<<< HEAD
         private IQueryable<ArtistModel> _artists;
         private IQueryable<ArtistModel> Artists
         {
             get { return  _artists ?? Db.Artists.AsQueryable(); }
         }
-=======
        
         public ViewResult UnapprovedArt()
         {
@@ -67,7 +62,6 @@ namespace Web.Controllers
             return View("UnapprovedArt", GetUnapprovedArt());
         }
 
->>>>>>> 9a70e7daccb592930f3d89606a68fd5beae33a3a
         //
         // GET: /Artist/
 
@@ -76,11 +70,7 @@ namespace Web.Controllers
             //List<ArtistModel> artists = Db.Artists.ToList();
             LoadDropDowns(null, null);
 
-<<<<<<< HEAD
             return View(Artists);
-=======
-            return View(Db.Artists.ToList());
->>>>>>> 9a70e7daccb592930f3d89606a68fd5beae33a3a
         }
 
         // GET: /Artist/Details/5
@@ -118,10 +108,7 @@ namespace Web.Controllers
 
         //
         // GET: /Artist/Edit/5
-<<<<<<< HEAD
-=======
 
->>>>>>> 9a70e7daccb592930f3d89606a68fd5beae33a3a
         public ActionResult Edit(int id)
         {
             ArtistModel artistmodel = Db.Artists.Find(id);
@@ -176,9 +163,7 @@ namespace Web.Controllers
             base.Dispose(disposing);
         }
 
-<<<<<<< HEAD
 
-=======
         public JsonResult Find(string searchTerms = "")
         {
             //firstname
@@ -213,6 +198,6 @@ namespace Web.Controllers
                 Count = count
             }, JsonRequestBehavior.AllowGet);
         }
->>>>>>> 9a70e7daccb592930f3d89606a68fd5beae33a3a
+
     }
 }
