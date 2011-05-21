5bb825769c26a24663894657ef2aa39c7b13fe51
Shawn Wallace <shawnewallace@gmail.com>
2011-05-20 23:46:46 -0400

    added email regex to constants for use

--- Web/Helpers/Constants.cs
+++ Web/Helpers/Constants.cs
@@ -10,5 +10,6 @@ namespace Web.Helpers
         //regest
         public const string REGEX_PHONE_NUMBER = @"^\+?\(?\d+\)?(\s|\-|\.)?\d{1,3}(\s|\-|\.)?\d{4}[\s]*[\d]*$";
         public const string REGEX_ZIP_CODE = @"^\d{5}([\-]\d{4})?$";
+        public const string REGEX_EMAIL_ADDRESS = @"([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})";
     }
 }
