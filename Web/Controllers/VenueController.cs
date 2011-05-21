d509493dffcbf596ad93b1c8ef86bfe0b35695e3
Shawn Wallace <shawnewallace@gmail.com>
2011-05-20 22:27:15 -0400

    updated controller base

--- Web/Controllers/VenueController.cs
+++ Web/Controllers/VenueController.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System.Data;
+using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Web.Models;
@@ -12,6 +13,90 @@ namespace Web.Controllers
             var model = Db.Venues.Select(v => v).ToList();
 
             return View("Index", model);
+        }
+
+        //
+        // GET: /Venue/Details/5
+
+        public ViewResult Details(int id)
+        {
+            VenueModel model = Db.Venues.Find(id);
+            return View(model);
+        }
+
+        //
+        // GET: /Venue/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Venue/Create
+
+        [HttpPost]
+        public ActionResult Create(VenueModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                Db.Venues.Add(model);
+                Db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(model);
+        }
+
+        //
+        // GET: /Venue/Edit/5
+
+        public ActionResult Edit(int id)
+        {
+            VenueModel model = Db.Venues.Find(id);
+            return View(model);
+        }
+
+        //
+        // POST: /Venue/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(VenueModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                Db.Entry(model).State = EntityState.Modified;
+                Db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(model);
+        }
+
+        //
+        // GET: /Venue/Delete/5
+
+        public ActionResult Delete(int id)
+        {
+            VenueModel model = Db.Venues.Find(id);
+            return View(model);
+        }
+
+        //
+        // POST: /Venue/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            VenueModel model = Db.Venues.Find(id);
+            Db.Venues.Remove(model);
+            Db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            Db.Dispose();
+            base.Dispose(disposing);
         }
     }
 }
