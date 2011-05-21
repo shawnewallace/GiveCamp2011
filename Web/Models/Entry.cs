fbba6aae22a1edf088ee7d9608644e783dced3b8
Aniket Braganza <aabragan@gmail.com>
2011-05-20 20:27:37 -0400

    Added Entry class as default datatype

---
+++ Web/Models/Entry.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Web.Models
+{
+    public class Entry<T>
+    {
+        public T Data { get; set; }
+        public bool Enabled { get; set; }
+    }
+}
