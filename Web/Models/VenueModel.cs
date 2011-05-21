6af17573a2512d8b3e156e7f61974df7be35ba41
Shawn Wallace <shawnewallace@gmail.com>
2011-05-20 23:13:59 -0400

    added create page

--- Web/Models/VenueModel.cs
+++ Web/Models/VenueModel.cs
@@ -1,13 +1,18 @@
-﻿namespace Web.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Web.Models
 {
     public class VenueModel
     {
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
         public string Address { get; set; }
         public string City { get; set; }
         public string State { get; set; }
         public string Zip { get; set; }
+
+        [RegularExpression(@"^\+?\(?\d+\)?(\s|\-|\.)?\d{1,3}(\s|\-|\.)?\d{4}[\s]*[\d]*$")]
         public string PhoneNumber { get; set; }
     }
 }
