e5df1b5dee63beff7e0f2745c89ee02157be1ecd
Aaron Ponzani <aponzani@gmail.com>
2011-05-22 14:14:31 -0400

    Add artist public profile

--- Web/Controllers/ArtistPublicController.cs
+++ Web/Controllers/ArtistPublicController.cs
@@ -7,27 +7,36 @@ using Web.Models;
 
 namespace Web.Controllers
 {
-    public class ArtistPublicController : ColumbusGiveCamp2011ControllerBase
-    {
-        //
-        // GET: /ArtistPublic/
+	public class ArtistPublicController : ColumbusGiveCamp2011ControllerBase
+	{
+		//
+		// GET: /ArtistPublic/
 
-        public PartialViewResult CoverFlow()
-        {
-            return PartialView("_CoverFlow", GetAllCoverFlowArt());
-        }
+		public PartialViewResult CoverFlow()
+		{
+			return PartialView("_CoverFlow", GetAllCoverFlowArt());
+		}
 
-        public ViewResult Profile(int id)
+		public ViewResult Profile(int id)
         {
             var x = Db.Artists
                 .Where(a => a.Id == id)
                 .Select(a => new ProfileModel
                                  {
-                                     Address = (a.Address1.Enabled ? a.Address1.Data : "")
+                                    Address = (a.Address1.Enabled ? a.Address1.Data : ""),
+									Name = a.FirstName + " " + a.LastName,
+									Bio = a.Biography,
+									Groups = "",
+									Awards = "",
+									Category = a.ArtistSubType.ArtistSubType,
+									EmailAddress = a.Email.Data,
+									PhoneNumber = (a.Phone.Enabled ? a.Phone.Data : ""),
+									City = a.City.Data,
+									State = a.State.Data,
+									ZipCode = a.Zip.Data
+								 });
 
-                                 });
-
-            return View();
+            return View(x.First());
         }
-    }
+	}
 }
