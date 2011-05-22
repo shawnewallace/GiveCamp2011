ec7952c54dcb5bacc0549993794c0dba68dac7d4
Aniket Braganza <aabragan@gmail.com>
2011-05-21 20:23:22 -0400

    Working on Create Artist

--- Web/Models/CustomRoleProvider.cs
+++ Web/Models/CustomRoleProvider.cs
@@ -11,8 +11,8 @@ namespace Web.Models
         public override string[] GetRolesForUser(string username)
         {
             string adminName = System.Configuration.ConfigurationManager.AppSettings["adminUserName"];
-               
-            if (username == adminName || username == "shawnwallace")
+
+            if (username == adminName || username == "shawnwallace" || username == "aabragan")
             {
                 return new string[] { "Admin" };
             }
